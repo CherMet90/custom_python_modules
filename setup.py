04c85c414a0b387633dcd35e7c508abd2e4cc169
CherMet90 <34270863+CherMet90@users.noreply.github.com>
2025-08-04 16:58:59 +0300

    minor restructuring for containerization

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 import setuptools
+
 setuptools.setup(
     # Includes all other files that are within your project folder 
     include_package_data=True,
-  
+
     # Name of your Package
     name='custom-modules',
- 
+
     # Project Version
-    version='1.3.1',
-     
+    version='1.3.2',
+
     # Website for your Project or Github repo
     url="https://github.com/CherMet90/custom_python_modules",
- 
-    # Projects you want to include in your Package
-    packages=setuptools.find_packages (),
-    
+
+    # Автоматически найдет пакет custom_modules и все его подпакеты (включая oid)
+    packages=setuptools.find_packages(),
+
     # Dependencies/Other modules required for your package to work
     install_requires=[
-    'setuptools==68.2.2',
-    'pynetbox==7.2.0',
-    'colorama==0.4.6',
-    'paramiko==3.3.1',
-    'prettytable==3.9.0',
-    'transliterate==1.10.2',
-    'ratelimit==2.2.1',
-    'backoff==2.2.1',
-    'python-gitlab==6.2.0',
-    'python-dotenv==1.0.0'
+        'setuptools==68.2.2',
+        'pynetbox==7.2.0',
+        'colorama==0.4.6',
+        'paramiko==3.3.1',
+        'prettytable==3.9.0',
+        'transliterate==1.10.2',
+        'ratelimit==2.2.1',
+        'backoff==2.2.1',
+        'python-gitlab==6.2.0',
+        'python-dotenv==1.0.0'
     ],
- 
 )
